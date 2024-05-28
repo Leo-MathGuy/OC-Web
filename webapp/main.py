f0af86f8ae42f7c1fb4d304b1fa206f3c193ef4c
Leo-MathGuy <leo.mathguy@gmail.com>
2024-05-28 13:08:58 +0500

    licence

--- webapp/main.py
+++ webapp/main.py
@@ -1,3 +1,21 @@
+"""    
+    Open Cosmos Website - Website for the Open Cosmos OSS Spaceflight Sim, based on KSP
+    Copyright (C) 2024, OC Community
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path="/static/")
